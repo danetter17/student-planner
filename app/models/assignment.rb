a160385f1df9e1da896b9d7d2743c70b9ce47e6e
danetter17 <etter.dt@gmail.com>
2019-07-31 16:22:06 -0500

    Add basic models

---
+++ app/models/assignment.rb
@@ -0,0 +1,4 @@
+class Assignment < ActiveRecord::Base
+  has_many :subjects
+  has_many :students, through: :subjects
+end
---
+++ app/models/subject.rb
@@ -0,0 +1,4 @@
+class Subject < ActiveRecord::Base
+  belongs_to :student
+  belongs_to :assignment
+end
