6de22226b404ba1c0fa984e7f524768a594846c5
danetter17 <etter.dt@gmail.com>
2019-08-01 16:18:25 -0500

    Edit routes, start adding new student forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/signin', to: 'sessions#new'
-  post '/signin', to: 'sessions#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
 
   root 'welcome#home'
